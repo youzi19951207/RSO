585b6f27b4721dd3fadca7c19bc3f1e5b8a11767
youzi19951207 <46488091+youzi19951207@users.noreply.github.com>
2020-04-17 23:32:20 +0800

    Update Generate test XOR data_different keys.py

--- Experiment3/Generate test XOR data_different keys.py
+++ Experiment3/Generate test XOR data_different keys.py
@@ -1,23 +1,19 @@
 import numpy as np
 
-CH=np.load("../data/#64_b64_100000_challenge.npy")
+CH=np.load("E:/data/#64_b64_100000_challenge_8XOR.npy")
 CH=np.asarray(CH,int)
-RSP=np.load("../data/#64_b64_100000_response.npy").transpose()
+RSP=np.load("E:/data/#64_b64_100000_response.npy").transpose()
 RSP=np.asarray(RSP,int)
 
 print(CH)
 
-
 Numbit=64
 NUM_PUF=64
-NumKey=30
-
-
-
+NumKey=32
 
 NUM_CRP=100000
 
-#============产生key=======================
+#============Generate key=======================
 key=np.random.randint(0,2,[NumKey,Numbit])
 #key=np.array([[1,1,0,1,0,1,0,0,1,0,0,1,0,0,1,0,0,0,1,0,1,0,0,0,0,1,1,1,1,1,0,0,1,1,0,0,0,1,0,0,0,0,0,1,1,1,1,1,0,0,1,1,0,0,1,1,0,0,1,0,0,0,0,1],
 #             [0,1,1,0,1,0,1,1,0,1,1,0,1,1,0,1,1,1,0,1,0,1,1,1,1,0,0,0,0,0,1,1,0,0,1,1,1,0,1,1,1,1,1,0,0,0,0,0,1,1,0,0,1,1,0,0,1,1,0,1,1,1,1,0]])
@@ -26,14 +22,12 @@ np.save("../data/#"+ str(NumKey) +"key",key)
 #=========================================
 
 
-
-
-#===================混淆challenge和response==================
+#===================obfuscate challenge and response==================
 CH_XOR=np.zeros((NUM_CRP,Numbit))
 RSP_XOR=np.zeros((NUM_CRP,NUM_PUF))
 
 for i in range(NUM_CRP):
-    # 从中随机选择一个key进行异或
+    # Randomly select a key from it for XOR
     key_choose = key[np.random.randint(0, NumKey)]
     for j in range(Numbit):
         a=CH[i,j]
@@ -42,7 +36,7 @@ for i in range(NUM_CRP):
         CH_XOR[i,j]=c
 
 for i in range(NUM_CRP):
-    # 从中随机选择一个key进行异或
+    # Randomly select a key from it for XOR
     key_choose = key[np.random.randint(0, NumKey)]
     for j in range(Numbit):
         a=RSP[i,j]
