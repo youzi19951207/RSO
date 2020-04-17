ad567c04286b25db7a7c95698f96153469510328
youzi19951207 <46488091+youzi19951207@users.noreply.github.com>
2020-04-17 23:09:31 +0800

    Update LR.py

--- Experiment2/LR.py
+++ Experiment2/LR.py
@@ -7,12 +7,12 @@ ans_f = []
 
 for key in range(0,32,2):
  
-#===============文件夹设置===================
+#===============Folder settings===================
     CH=np.load("../data/#64_b64_100000_challenge_8XOR.npy")
     #RSP=np.load('G:/Data/Data64_error/#1_b64_100000_response_10%.npy').transpose()
     RSP=np.load('../data/#64_b64_100000_response_8XOR.npy')
 
-    #==================参数设置==================
+    #==================parameter settings==================
 
     ans = []
     NUM_P = 64
@@ -22,12 +22,12 @@ for key in range(0,32,2):
         NUM_CHBIT=64
         NUM_TRAIN = 90000
 
-        #=============生成测试集和训练集==============
-        tf.reset_default_graph()  # 重置默认图
-        graph = tf.Graph()        # 新建空白图
-        with graph.as_default() as g:   # 将新建的图作为默认图
+        #=============Generate test set and training set==============
+        tf.reset_default_graph()  # Reset the default image
+        graph = tf.Graph()        # New blank image
+        with graph.as_default() as g:   # Use the newly created image as the default image
             with tf.Session(graph=g):
-                #将challenges做处理提取出特征
+                # Process the challenges to extract features
                 X_train=CH
                 temp=1-2*X_train
                 temp01 = np.array([np.prod(temp[:, i:], 1) for i in range(NUM_CHBIT)]).transpose()
@@ -41,20 +41,15 @@ for key in range(0,32,2):
 
                 #print(np.shape(x_train))
                 #print(np.shape(y_train))
-                #设置训练参数
-
+                #Set training parameters
 
                 learning_rate = 0.01
-
-
 
                 theta = tf.Variable(tf.zeros([NUM_CHBIT, NUM_PUF]))
                 theta0 = tf.Variable(tf.zeros([1, NUM_PUF]))
                 y = 1 / (1 + tf.exp(-(tf.matmul(x_train, theta) + theta0)))
                 loss = tf.reduce_mean(- y_train * tf.log(y) - (1 - y_train) * tf.log(1 - y))
                 train = tf.train.AdamOptimizer(learning_rate).minimize(loss)
-
-
 
                 init = tf.global_variables_initializer()
                 sess = tf.Session()
@@ -71,16 +66,16 @@ for key in range(0,32,2):
                         maxx = max(maxx , a)
                         if a < 0.1:
                             break
-                        print('经过%d轮训练准确率为：' % (int(step) / 500+ 1), a)
+                        print('After %d training times, the accuracy rate is：' % (int(step) / 500+ 1), a)
                     # if (a>=0.95):
                     #     break;
-                #print('特征值为：', sess.run(theta).flatten(), sess.run(theta0).flatten())
+                #print('Characteristic value is：', sess.run(theta).flatten(), sess.run(theta0).flatten())
                 #print(np.shape(sess.run(theta)),np.shape(sess.run(theta0)))
-                print("%d个训练集预测准确率为:",maxx)
+                print("%d training set prediction accuracy rate is:",maxx)
                 ans.append(maxx)
     print(ans)
     print(sum(ans)/NUM_P)
     ans_f.append(sum(ans)/NUM_P)
 
 
-np.save("E:/万露/data_XOR",ans_f)
+np.save("E:/data_XOR",ans_f)
