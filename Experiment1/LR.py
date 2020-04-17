dc83a7a85905081ca926b6856f3fea268d2dafdd
youzi19951207 <46488091+youzi19951207@users.noreply.github.com>
2020-04-17 22:33:16 +0800

    Update LR.py

--- Experiment1/LR.py
+++ Experiment1/LR.py
@@ -5,11 +5,11 @@ import time
 
 NUM_KEY = 8
 
-#===============文件夹设置===================
-CH=np.load("E:/万露的实验/data/#64_b64_100000_challenge_"+str(NUM_KEY)+"XOR.npy","r")
-RSP=np.load("E:/万露的实验/data/#64_b64_100000_response_"+str(NUM_KEY)+"XOR.npy","r")
-# CH=np.load("D:/万露的实验/data/#64_b64_100000_challenge.npy")
-# RSP=np.load("D:/万露的实验/data/#64_b64_100000_response.npy").transpose()
+#===============Folder settings===================
+CH=np.load("E:/data/#64_b64_100000_challenge_"+str(NUM_KEY)+"XOR.npy","r")
+RSP=np.load("E:/data/#64_b64_100000_response_"+str(NUM_KEY)+"XOR.npy","r")
+# CH=np.load("D:/data/#64_b64_100000_challenge.npy")
+# RSP=np.load("D:/data/#64_b64_100000_response.npy").transpose()
 
 
 
@@ -23,19 +23,18 @@ for NN in range(5,6):
     with graph.as_default() as g:
         with tf.Session(graph=g):
 
-
             NUM_TRAIN = NUM_Z[NN]
 
-
-            #==================参数设置==================
+            #==================parameter settings==================
             NUM_PUF=64
             NUM_CHBIT=64
             Num_step=10000
             fig=plt.figure()
             ax=plt.subplot(111)
-            #=============生成测试集和训练集==============
+            
+            #=============Generate test set and training set==============
 
-            #将challenges做处理提取出特征
+            #Process the challenges to extract features
             X_train=CH
             temp=1-2*X_train
             temp01 = np.array([np.prod(temp[:, i:], 1) for i in range(NUM_CHBIT)]).transpose()
@@ -47,11 +46,8 @@ for NN in range(5,6):
             y_test=RSP[90000:100000]
             y_test=2*y_test-1
 
-
-            #设置训练参数
-
+            # Set training parameters
             learning_rate = 0.1
-
 
             theta = tf.Variable(tf.zeros([NUM_CHBIT, NUM_PUF]))
             theta0 = tf.Variable(tf.zeros([1, NUM_PUF]))
@@ -64,7 +60,7 @@ for NN in range(5,6):
             sess = tf.Session()
             sess.run(init)
 
-            #startime=time.time()
+            # startime=time.time()
             A=[]
             B=[]
 
@@ -72,13 +68,13 @@ for NN in range(5,6):
                 sess.run(train)
                 if step %50==0:
                     A.append(step)
-                    #print('经过%d轮训练cost为：'%(int(step)/50+1),sess.run(loss))
+                    #print('After %d times training, the cost is：'%(int(step)/50+1),sess.run(loss))
                     pred = tf.matmul(x_test, theta) + theta0
                     correct_prediction = tf.equal(tf.sign(pred), y_test)
                     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
                     a=sess.run(accuracy)
                     B.append(a)
-                    print("准确率为：")
+                    print("Accuracy rate：")
                     #siz = np.shape(sess.run(pred))
                     #print(siz)
                     #print(sess.run(tf.reduce_sum(tf.cast(correct_prediction, tf.float32))))
@@ -87,9 +83,9 @@ for NN in range(5,6):
                 #     break;
             #endtime=time.time()
             #time=endtime-startime
-            #print('特征值为：', sess.run(theta).flatten(), sess.run(theta0).flatten())
+            #print('Characteristic value：', sess.run(theta).flatten(), sess.run(theta0).flatten())
             #print(np.shape(sess.run(theta)),np.shape(sess.run(theta0)))
-            print(NUM_TRAIN,"个训练集预测准确率为:",sess.run(accuracy))
+            print(NUM_TRAIN,"The training set prediction accuracy is:",sess.run(accuracy))
             #print(time)
             #plt.plot(A,B,'c-',label='LR')
             #plt.xlabel("Number of iterations")
