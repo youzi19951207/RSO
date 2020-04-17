7eeddc2eb50d298d8c1710dedbfa4baa87397765
youzi19951207 <46488091+youzi19951207@users.noreply.github.com>
2020-04-17 22:25:40 +0800

    Update Generate test XOR data_different keys.py

--- Experiment1/Generate test XOR data_different keys.py
+++ Experiment1/Generate test XOR data_different keys.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-CH=np.load("D:/万露的实验/data/#64_b64_100000_challenge.npy")
+CH=np.load("D:/data/#64_b64_100000_challenge.npy")
 CH=np.asarray(CH,int)
-RSP=np.load("D:/万露的实验/data/#64_b64_100000_response.npy").transpose()
+RSP=np.load("D:/data/#64_b64_100000_response.npy").transpose()
 RSP=np.asarray(RSP,int)
 
 print(CH)
@@ -17,23 +17,23 @@ NumKey=32
 
 NUM_CRP=100000
 
-#============产生key=======================
+#============Generate key=======================
 key=np.random.randint(0,2,[NumKey,Numbit])
 #key=np.array([[1,1,0,1,0,1,0,0,1,0,0,1,0,0,1,0,0,0,1,0,1,0,0,0,0,1,1,1,1,1,0,0,1,1,0,0,0,1,0,0,0,0,0,1,1,1,1,1,0,0,1,1,0,0,1,1,0,0,1,0,0,0,0,1],
 #             [0,1,1,0,1,0,1,1,0,1,1,0,1,1,0,1,1,1,0,1,0,1,1,1,1,0,0,0,0,0,1,1,0,0,1,1,1,0,1,1,1,1,1,0,0,0,0,0,1,1,0,0,1,1,0,0,1,1,0,1,1,1,1,0]])
 print(key)
-np.save("D:/万露的实验/data/#"+ str(NumKey) +"key",key)
+np.save("D:/data/#"+ str(NumKey) +"key",key)
 #=========================================
 
 
 
 
-#===================混淆challenge和response==================
+#===================Obfuscate challenge and response==================
 CH_XOR=np.zeros((NUM_CRP,Numbit))
 RSP_XOR=np.zeros((NUM_CRP,NUM_PUF))
 
 for i in range(NUM_CRP):
-    # 从中随机选择一个key进行异或
+    # Randomly select a key from it for XOR
     key_choose = key[np.random.randint(0, NumKey)]
     for j in range(Numbit):
         a=CH[i,j]
@@ -42,7 +42,7 @@ for i in range(NUM_CRP):
         CH_XOR[i,j]=c
 
 for i in range(NUM_CRP):
-    # 从中随机选择一个key进行异或
+    # Randomly select a key from it for XOR
     key_choose = key[np.random.randint(0, NumKey)]
     for j in range(Numbit):
         a=RSP[i,j]
@@ -68,5 +68,5 @@ print(np.shape(CH_XOR))
 
 # print(CH_XOR_LFSR)
 # print(np.shape(CH_XOR_LFSR))
-np.save("D:/万露的实验/data/#64_b64_100000_challenge_"+ str(NumKey) +"XOR",CH_XOR)
-np.save("D:/万露的实验/data/#64_b64_100000_response_"+ str(NumKey) +"XOR",RSP_XOR)
+np.save("D:/data/#64_b64_100000_challenge_"+ str(NumKey) +"XOR",CH_XOR)
+np.save("D:/data/#64_b64_100000_response_"+ str(NumKey) +"XOR",RSP_XOR)
